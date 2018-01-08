f53daab89bffe73f26ededc1856b2658f692cd1d
Aidan Christopher Haughey <nazghoul22@gmail.com>
2018-01-07 16:52:10 -0800

    project basically done with databse active, making final aesthetic touch ups

--- index.vr.js
+++ index.vr.js
@@ -26,10 +26,7 @@ export default class ButterflyVR extends React.Component {
   componentWillMount() {
     /* Create reference to scores in Firebase Database */
     let scoresRef = fire.database().ref('scores').orderByValue().limitToFirst(3);
-    scoresRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database */
-      let score = snapshot.val();
-      this.setState({ scores: [score].concat(this.state.scores) });
+
     })
   }
 
